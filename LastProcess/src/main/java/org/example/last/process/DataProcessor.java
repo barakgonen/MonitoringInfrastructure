e47f56109ad56e86608dc511bcc543fc82180efa
Barak Gonen <barakgonen9@gmail.com>
2022-02-19 06:10:53 -0800

    Last process without sleep

--- LastProcess/src/main/java/org/example/last/process/DataProcessor.java
+++ LastProcess/src/main/java/org/example/last/process/DataProcessor.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.errors.SerializationException;
 import org.bg.avro.structures.base.objects.CoordinateWithId;
 
+import java.time.Instant;
 import java.util.function.Consumer;
 
 public class DataProcessor<K, V> implements Consumer<ConsumerRecord<K, V>> {
@@ -18,17 +19,14 @@ public class DataProcessor<K, V> implements Consumer<ConsumerRecord<K, V>> {
 
     @Override
     public void accept(ConsumerRecord<K, V> kvConsumerRecord) {
-        if (kvConsumerRecord.value() instanceof CoordinateWithId) {
-            try {
-                Thread.sleep(100);
-                System.out.println("Last process, id: " + kvConsumerRecord.key());
-                ProducerRecord<String, CoordinateWithId> record =
-                        new ProducerRecord(outputTopic, kvConsumerRecord.key(), kvConsumerRecord.value());
-                producer.send(record);
-            } catch (InterruptedException | SerializationException e) {
-                System.out.println("ERROR!!!! " + e.getMessage());
-                e.printStackTrace();
-            }
+        try {
+            System.out.println("Last process, id: " + kvConsumerRecord.key() + ", total time: " + (Instant.now().toEpochMilli() - ((CoordinateWithId)kvConsumerRecord.value()).getId().getStartTimeMillis()) + " millis");
+            ProducerRecord<String, CoordinateWithId> record =
+                    new ProducerRecord(outputTopic, kvConsumerRecord.key(), kvConsumerRecord.value());
+            producer.send(record);
+        } catch (SerializationException e) {
+            System.out.println("ERROR!!!! " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
--- LastProcess/src/main/java/org/example/last/process/MyKafkaConsumer.java
+++ LastProcess/src/main/java/org/example/last/process/MyKafkaConsumer.java
@@ -1,11 +1,15 @@
 package org.example.last.process;
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class MyKafkaConsumer<K, V> extends KafkaConsumer<K, V> {
@@ -20,11 +24,12 @@ public class MyKafkaConsumer<K, V> extends KafkaConsumer<K, V> {
     private static Properties getProps() {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group6");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
         props.put("schema.registry.url", "http://localhost:8081");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
         return props;
     }
 
@@ -36,7 +41,9 @@ public class MyKafkaConsumer<K, V> extends KafkaConsumer<K, V> {
                     invoker.accept(record);
                 }
             }
-        } finally {
+        } catch (Exception e) {
+            System.out.println("Bg: " + e.getMessage());
+        } finally{
             this.close();
         }
     }
