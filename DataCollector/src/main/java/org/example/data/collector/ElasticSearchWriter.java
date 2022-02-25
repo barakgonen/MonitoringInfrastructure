1a82c5ab2416225f4b5d13d92c079280334b11a5
Barak Gonen <barakgonen9@gmail.com>
2022-02-25 07:03:45 -0800

    refactor to single multipurpose service

--- DataCollector/src/main/java/org/example/data/collector/ElasticSearchWriter.java
+++ DataCollector/src/main/java/org/example/data/collector/ElasticSearchWriter.java
@@ -1,2 +1,93 @@
-package org.example.data.collector;public class ElasticSearchWriter {
+package org.example.data.collector;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.avro.Schema;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class ElasticSearchWriter<K, V> {
+    private Collection<String> topics;
+    private RestClientBuilder elasticsearchClient;
+    private RestHighLevelClient hlrc;
+
+    public ElasticSearchWriter(Collection<String> topics) {
+        this.topics = topics;
+        final CredentialsProvider credentialsProvider =
+                new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "Aa123456"));
+
+        this.elasticsearchClient = RestClient.builder(
+                        new HttpHost("localhost", 9200))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(
+                            HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder
+                                .setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+        this.hlrc = new RestHighLevelClient(elasticsearchClient);
+    }
+
+    public void createIndexes() {
+        this.topics.forEach(s -> createIndex(s.toLowerCase(Locale.ROOT)));
+    }
+
+    private void createIndex(String indexName) {
+
+        try {
+            CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
+//            HashMap<String, String> mapping = new HashMap<>();
+//            createIndexRequest.mapping(mapping);
+            hlrc.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeToIndex(ConsumerRecords<K, V> kvConsumerRecords) {
+        ObjectMapper mapper = new ObjectMapper();
+        BulkRequest request = new BulkRequest();
+        try {
+            for (ConsumerRecord<K, V> kvConsumerRecord : kvConsumerRecords) {
+                Map<String, String> map = mapper.readValue(kvConsumerRecord.value().toString(), Map.class);
+
+                request.add(
+                        new IndexRequest(kvConsumerRecord.topic().toLowerCase(Locale.ROOT))
+                                .id(String.valueOf(kvConsumerRecord.offset()))
+                                .source(map));
+
+
+            }
+
+            BulkResponse response =hlrc.bulk(request, RequestOptions.DEFAULT);
+            System.out.println("took: " + response.getTook() + ", request.size: " + request.requests().size());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/org/bg/example/generic/process/MyKafkaConsumer.java
@@ -0,0 +1,50 @@
+package org.bg.example.generic.process;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public class MyKafkaConsumer<K, V> extends KafkaConsumer<K, V> {
+
+    private Consumer<ConsumerRecord<K, V>> invoker;
+
+    public MyKafkaConsumer(Consumer<ConsumerRecord<K, V>> consumer) {
+        super(getProps());
+        this.invoker = consumer;
+    }
+
+    private static Properties getProps() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        props.put("schema.registry.url", "http://localhost:8081");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        return props;
+    }
+
+    public void startConsume() {
+        try {
+            while (true) {
+                ConsumerRecords<K, V> records = this.poll(Duration.of(50, ChronoUnit.MILLIS));
+                for (ConsumerRecord<K, V> record : records) {
+                    invoker.accept(record);
+                }
+            }
+        } finally {
+            this.close();
+        }
+    }
+}
